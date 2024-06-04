8ccc9b49397d48a79451cfb4c9e99ef76081f1ac
nitindahiya199 <nitindahiya00000@gmail.com>
2024-06-04 18:05:50 +0530

    more rust practice

--- src/main.rs
+++ src/main.rs
@@ -83,14 +83,102 @@
 
 // -------------------------------------------------------------------------------------
 
-fn main() {
-    run_loop();
+// fn main() {
+//     run_loop();
+// }
+
+// fn run_loop() {
+//     let mut x = Vec::new();
+//     for i in 0..100 {
+//         x.push(i);
+//     }
+//     return  println!("{:?}",x) ;
+// }
+
+// -------------------------------------------------------------------------------------
+
+// Memory Management
+
+// fn main() {
+//     stack_fn();   // Call the function that uses stack memory
+//     heap_fn();    // Call the function that uses heap memory
+//     update_string();  // Call the function that changes size of variable at runtime
+// }
+
+// fn stack_fn() {
+//     // Declare a few integers on the stack
+//     let a: i32 = 10;
+//     let b: i32 = 20;
+//     let c: i32 = a + b;
+//     println!("Stack function: The sum of {} and {} is {}", a, b, c);
+// }
+
+// fn heap_fn() {
+//     // Create a string, which is allocated on the heap
+//     let s1: String = String::from("Hello");
+//     let s2: String = String::from("World");
+//     let combined: String = format!("{} {}", s1, s2);
+//     println!("Heap function: Combined string is '{}'", combined);
+// }
+
+// fn update_string() {
+//     // Start with a base string on the heap
+//     let mut s: String = String::from("Initial string");
+//     println!("Before update: {}", s);
+//     println!("Capacity:{}, Length: {}, pointer: {:p}", s.capacity(),s.len(),s.as_ptr());
+
+//     // Append some text to the string
+//     s.push_str(" and some additional text");
+//     println!("After update: {}", s);
+//     println!("Capacity:{}, Length: {}, pointer: {:p}", s.capacity(),s.len(),s.as_ptr());
+// }
+
+// -------------------------------------------------------------------------------------------
+
+// Ownership
+
+// fn main() {
+//     let x: i32 = 1; // crated on stack, owner is main fn
+//     let y = 3; // created on stack, owner is main fn
+//     println!("{}", sum(x, y));
+//     println!("Hello, world!");
+// }
+
+// fn sum(a: i32, b: i32) -> i32 {
+//     let c = a + b; // owner of c ,a ,b is sum fn
+//     return c;
+// }
+
+// fn main(){
+//     let x =String::from("Hii");
+//     take_ownership(x);
+//     // print!("{}", x); // can not use here because of ownership
+// }
+// fn take_ownership(some_string:String){ // some_string takes the ownership, x is no longer it's owner.
+//     print!("{}", some_string);
+// }
+
+// fn main() {
+//     let mut x = String::from("Hii");
+//     x = take_ownership(x);
+//     print!("{}", x); // can use here because ownership return to the caller
+// }
+// fn take_ownership(some_string: String) -> String {
+//     // some_string takes the ownership, x is no longer it's owner.
+//     print!("{}", some_string);
+//     return some_string; // ownership return to the caller;
+// }
+
+// --------------------------------------------------------------------------------
+
+// Borrowing and references
+
+fn main(){
+    let mut str: String = String::from("Hello ");
+    update_str(&mut str);
+    print!("{}",str);
 }
 
-fn run_loop() {
-    let mut x = Vec::new();
-    for i in 0..100 {
-        x.push(i);
-    }
-    return  println!("{:?}",x) ; 
+fn update_str(str: &mut String){
+str.push_str("World");
 }
